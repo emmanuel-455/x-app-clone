9966f4d8664622c12dc105ebf4e6d5c93c8a2944
Ngene Arinzechukwu <120824375+emmanuel-455@users.noreply.github.com>
2025-09-01 12:27:57 +0100

    startserver function added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,19 @@ import { connectDB } from "./config/db.js"
 
 const app = express()
 
-connectDB();
 
-app.get("/", (req, res) => res.send("hi"))
+const startServer = async () => {
+  try {
+    await connectDB();
 
-app.listen(ENV.PORT, () => {
-    console.log("Server running on port", ENV.PORT)
-})
+    // listen for local development
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+    }
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
